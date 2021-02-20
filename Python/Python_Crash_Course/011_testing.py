84e9f1eb8ab46e1bca950594fba0e4a5178b0c8f
Bruno Grego <brunosilvagrego@gmail.com>
2021-02-20 18:58:54 +0000

    Python Crash Course: testing

---
+++ Python/Python_Crash_Course/011_testing.py
@@ -0,0 +1,88 @@
+import unittest
+
+### Testing a function ###
+
+def get_formatted_name(first, last, middle=''):
+    """Generate a neatly formatted full name."""
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
+
+    return full_name.title()
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
+
+### Testing a Class ###
+
+class AnonymousSurvey():
+    """Collect anonymous answers to a survey question."""
+
+    def __init__(self, question):
+        """Store a question, and prepare to store responses."""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """Show the survey question."""
+        print(question)
+
+    def store_response(self, new_response):
+        """Store a single response to the survey."""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """Show all the responses that have been given."""
+        print("Survey results:")
+
+        for response in responses:
+            print('- ' + response)
+
+class TestAnonmyousSurvey(unittest.TestCase):
+    """Tests for the class AnonymousSurvey"""
+
+    def setUp(self):
+        """
+        Create a survey and a set of responses for use in all test methods.
+        """
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+        self.my_survey.store_response(self.responses[0])
+
+        self.assertIn(self.responses[0], self.my_survey.responses)
+    
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+
+### Execute all unit tests in this file ###
+
+# NOTE: Any method within a unittest.TestCase class that starts with 'test_'
+# will be run automatically when calling unittest.main()
+
+# NOTE: When a test case is running, Python prints one character for each unit
+# test as it is completed. A passing test prints a dot, a test that results in an
+# error prints an E, and a test that results in a failed assertion prints an F.
+
+unittest.main()
